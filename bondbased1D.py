90b74f47f5fea9b3c6df7596eb3074438c19f283
Patrick <patrickdiehl@lsu.edu>
2020-05-18 10:14:48 -0500

    Some changes

--- bondbased1D.py
+++ bondbased1D.py
@@ -1,7 +1,12 @@
+#Protoype for the 1D bond-based analytic stiffness matrix
+#@author Patrick Diehl
+#@date May 2020
 import numpy as np 
 from scipy import linalg
 
 nodes = []
+neighbors = []
+matrix = np.zeros((len(nodes),len(nodes)))
 
 h = 0.1
 delta = 3*h
@@ -11,14 +16,38 @@ beta = 1
 
 VB = h
 
+
 def length(x,y): 
+    """ Computes the length between to nodes
+
+    Keyword arguments:
+    x -- the node x
+    y -- the node y
+
+    return the length
+    """
     return np.sqrt((y-x) * (y-x)) 
 
 def S(i,j,u):
+    """ Computes the stretch between node x and nod y
+
+    Keyword arguments:
+    x -- the node x
+    y -- the node y
+    u -- the displacement vector 
+
+    return the stretch
+    """
     return  (u[j] - u[i]) / length(nodes[i],nodes[j])  *  (nodes[j]-nodes[i]) / (length(nodes[i],nodes[j]))
 
-
 def w(r):
+    """ Influence function
+
+    Keyword arguments:
+    r -- the initial length between node x and y
+
+    return the weight
+    """
     return 1
 
 def f1(r):
@@ -36,57 +65,48 @@ def L(i,j,u):
     return (2./VB) * w(length(nodes[i],nodes[j])/delta)  * f1(r) * ( 1. / length(nodes[i],nodes[j]))  *  (nodes[j]-nodes[i]) / (length(nodes[i],nodes[j]))
 
 
+def searchNeighbors():
+    for i in range(0,len(nodes)):
+        neighbors.append([])
+        for j in range(0,len(nodes)):
+            if i != j and abs(nodes[j]-nodes[i]) < delta:
+                neighbors[i].append(j)
+
+
+def assemblymatrix():
+    matrix = np.zeros((10,10))
+    for i in range(0,len(nodes)):
+        for j in neighbors[i]:
+            matrix[i][j] = A(i,j,u)
+            matrix[i][i] -= A(i,j,u)
+    return matrix
 
 # Generate the mesh
-
 n = int(1/h)
-
 nodes = np.linspace(0,1,n)
 
-#print(nodes)
-
 # Search neighbors
+searchNeighbors()
 
-neighbors = []
-
-for i in range(0,len(nodes)):
-    neighbors.append([])
-    for j in range(0,len(nodes)):
-        if i != j and abs(nodes[j]-nodes[i]) < delta:
-            neighbors[i].append(j)
-
-#print(neighbors)
-
-
-# Generate stiffness matrix
-
-matrix = np.zeros((len(nodes),len(nodes)))
-
+# Initialize 
 u = np.zeros(len(nodes))
 b = np.zeros(len(nodes))
 b[0] = -10
 b[len(nodes)-1] = 10
-f = np.zeros(len(nodes))
 
 
-for i in range(0,len(nodes)):
-    for j in neighbors[i]:
-        matrix[i][j] = A(i,j,u)
-        matrix[i][i] -= A(i,j,u)
-
-# Generate the left-hand side
-
-for i in range(0,len(nodes)):
-    f[i] = b[i]
-    for j in neighbors[i]:
-        f[i] += L(i,j,u)
-
-
-#print(matrix)
-#print(f)
-
-x = linalg.solve(matrix, f)
+for i in range (0,3):
+    # Assemble stiffness matrix
+    matrix = assemblymatrix()
+    f = np.zeros(len(nodes))
+    # Generate the left-hand side
+    for i in range(0,len(nodes)):
+        f[i] = b[i]
+        for j in neighbors[i]:
+            f[i] += L(i,j,u)
+    u = linalg.solve(matrix, f)
+    print(i)
 
 
 
-print(x)
+print(u)
