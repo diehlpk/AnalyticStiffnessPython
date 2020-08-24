23ff96f3eea76616ad77bd16f5178452d20965a6
Patrick <patrickdiehl@lsu.edu>
2020-08-24 15:25:13 -0500

    Update the force for the 2d case

--- bondbased2D.py
+++ bondbased2D.py
@@ -41,12 +41,12 @@ class Compute:
         for i in range(0,n):
             for j in range(0,n):
                 self.nodes.append([i*h,j*h])
-                if i*h < 1*h:
+                if i*h < self.delta_factor*h:
                     self.load.append(index)
-                    #plt.scatter(i*h,j*h)
+                    #plt.scatter(i*h,j*h,c="blue")
                 if i*h > 1.6-1*h:
                     self.fix.append(index)
-                    #plt.scatter(i*h,j*h)
+                    #plt.scatter(i*h,j*h,c="red")
                 index+=1
         #plt.show()
         
@@ -109,7 +109,7 @@ class Compute:
         self.b = np.zeros(2*len(self.nodes))
         for i in range(0,len(self.nodes)):
                 if i in self.load:
-                    self.b[2*i] = -0.4 
+                    self.b[2*i] = -40 / (1.6*self.h)
 
         self.f = np.zeros(2*len(self.nodes))
 
@@ -135,8 +135,8 @@ class Compute:
         for i in range(0,len(self.nodes)):
             for j in self.neighbors[i]:
                 tmp =  self.L(i,j)
-                self.f[2*i] += tmp[0] * self.V[i]
-                self.f[2*i+1] += tmp[1] * self.V[i]
+                self.f[2*i] += tmp[0] 
+                self.f[2*i+1] += tmp[1] 
         
             if i in self.fix:
                 self.f[2*i] = 0
@@ -229,16 +229,16 @@ class Compute:
             it += 1
 
     def ux(self,x):
-        F=-0.4
+        F=-40
         E=4000
-        W = L = 16
+        W = L = 1.6
         t = 1
         return F/(E*W*t)*(x-L)
 
     def uy(self,y):
-        F=-0.4
+        F=-40
         E=4000
-        W = L = 16
+        W = L = 1.6
         t = 1
         nu = 1/3
         return -(y/W-0.5) / E / t * nu * F
@@ -292,7 +292,7 @@ class Compute:
         plt.clf()
         # Force plots
         tmp = self.f.reshape((len(self.nodes),2)) 
-        plt.scatter(self.nodes[:,0],self.nodes[:,1],c=tmp[:,0])
+        plt.scatter(self.nodes[:,0],self.nodes[:,1],c=tmp[:,0] * self.V)
         ax = plt.gca()
         ax.set_facecolor('#F0F8FF')
         v = np.linspace(min(tmp[:,0]), max(tmp[:,0]), 10, endpoint=True)
@@ -303,7 +303,7 @@ class Compute:
         plt.savefig("bond-based-2d-f-x-"+str(self.h)+"-"+str(self.delta_factor)+".pdf",bbox_inches='tight')
         plt.clf()
         # Plot f_y
-        plt.scatter(self.nodes[:,0],self.nodes[:,1],c=tmp[:,1])
+        plt.scatter(self.nodes[:,0],self.nodes[:,1],c=tmp[:,1] * self.V)
         ax = plt.gca()
         ax.set_facecolor('#F0F8FF')
         v = np.linspace(min(tmp[:,1]), max(tmp[:,1]), 10, endpoint=True)
@@ -317,5 +317,5 @@ class Compute:
 if __name__=="__main__": 
 
     c = Compute(float(sys.argv[1]),int(sys.argv[2]))
-    c.solve(1000000,1e-6)
+    c.solve(1000000,1e-3)
     c.plot()
